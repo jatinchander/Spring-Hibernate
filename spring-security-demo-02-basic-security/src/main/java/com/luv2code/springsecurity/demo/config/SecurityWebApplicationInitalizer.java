1353fcc43a4a229ae3697de1ae816b71debd2a06
nycab <nycab@DESKTOP-E0J7298.myfiosgateway.com>
2020-10-07 14:42:02 -0400

    added spring security configuration classes

---
+++ spring-security-demo-02-basic-security/src/main/java/com/luv2code/springsecurity/demo/config/SecurityWebApplicationInitalizer.java
@@ -0,0 +1,8 @@
+// special class to register Spring Security Filters
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitalizer extends AbstractSecurityWebApplicationInitializer {
+
+}
