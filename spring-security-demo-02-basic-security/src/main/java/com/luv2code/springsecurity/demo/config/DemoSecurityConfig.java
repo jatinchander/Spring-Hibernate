4a5af3ea1ab1448f5c10178b5075393bc857d862
nycab <nycab@DESKTOP-E0J7298.myfiosgateway.com>
2020-10-07 16:02:09 -0400

    added login form

--- spring-security-demo-02-basic-security/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-02-basic-security/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.luv2code.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -11,11 +12,11 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	// Configure users (in memory, database, ldap, etc.)
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
 		// add our users for in memory authentication
-		
 		UserBuilder users = User.withDefaultPasswordEncoder();
 		
 		auth.inMemoryAuthentication()
@@ -24,6 +25,24 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 			.withUser(users.username("susan").password("test123").roles("ADMIN"));
 		
 	}
+
+	// Configure security of web paths in application, login, logout, etc.
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests() // restrict access based on the HTTPServletRequest
+			.anyRequest().authenticated() // user must be logged in
+			.and()
+			.formLogin() // custom login page
+			.loginPage("/showMyLoginPage") // show our custom login
+			// (Note: controller mapping is needed for this ^)
+			.loginProcessingUrl("/authenticateTheUser") // submit data & check credentials
+			// (Note: no controller mapping is needed for this ^)
+			.permitAll(); // allow everyone to see login page
+		
+	}
+	
+	
 	
 	
 
